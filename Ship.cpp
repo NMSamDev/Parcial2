1e5cee58a4204e5b5509949a54a2c62151658cdb
NoManchesam <A01730719@itesm.mx>
2020-05-07 19:09:10 -0500

    Apenas declarando funciones

--- Ship.cpp
+++ Ship.cpp
@@ -21,3 +21,36 @@ Ship::~Ship()
  {
 
  }
+
+void draw(){
+}
+
+void setRotation(bool){
+}
+
+void setTranslation(bool){
+}
+
+void setRotation(float axis[3], float deg){
+}
+
+void setAxisRotation(float axis[3]){
+}
+
+void setDeltaAxisRotation(float delta[3]){
+}
+
+void setDegRotation(float deg){
+}
+
+void setDeltaDegRotation(float delta){
+}
+
+void setAxisTranslation(float axis[3]){
+}
+
+void setDeltaAxisTranslation(float delta[3]){
+}
+
+void setAnimation(bool value){
+}
